470ecfcc2e9284f645ed425fb06ee546cd9e7190
pk <pappu.kumar@fluper.in>
2023-07-27 01:02:27 +0530

    updated md file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Crud-api-with-completable-future-and-executor-service
 Simple Crud API With Completable Future(CompletableFuture) and Executor Service(ExecutorService )
+
+# Here are some guidelines for deciding when to use @Async:
+```
+1. Use @Async for long-running or blocking methods.
+2. Do not use @Async for short or non-blocking methods.
+3. Consider using @Async for methods that are called frequently.
+4. Use @Async in conjunction with a thread pool to manage the number of concurrent threads.
+```
+
+CompletableFuture is a powerful tool for asynchronous programming in Java. It can be used to run tasks concurrently, chain tasks together, and handle errors.
+
+# Here are some situations when you might want to use CompletableFuture:
+```
+1. When you need to run multiple tasks concurrently. CompletableFuture makes it easy to run multiple tasks at the same time, without blocking the main thread. This can improve the performance and scalability of your application.
+2. When you need to chain tasks together. CompletableFuture provides a variety of methods for chaining tasks together. This can be useful for tasks that need to be executed in a specific order, or for tasks that need to be executed in parallel.
+3. When you need to handle errors. CompletableFuture provides a variety of methods for handling errors. This can help you to ensure that your application continues to run even if an error occurs.
+4. Here are some examples of when you might want to use CompletableFuture:
+
+5. When you are making multiple network calls. CompletableFuture can be used to make multiple network calls concurrently. This can improve the performance of your application by reducing the total amount of time it takes to make all of the calls.
+6. When you are processing a large file. CompletableFuture can be used to process a large file in parallel. This can improve the performance of your application by reducing the total amount of time it takes to process the file.
+7. When you are running a long-running task. CompletableFuture can be used to run a long-running task in a separate thread. This can improve the user experience by preventing the main thread from blocking.
+```
--- src/main/java/com/pk/app/service/BookService.java
+++ src/main/java/com/pk/app/service/BookService.java
@@ -1,9 +1,15 @@
 package com.pk.app.service;
 
+import com.pk.app.model.Book;
+import com.pk.app.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,157 +18,147 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-
-import com.pk.app.model.Book;
-import com.pk.app.repository.BookRepository;
-
 @Service
 public class BookService {
-	@Autowired
-    private BookRepository bookRepository;
-	
-    private ExecutorService executorService = Executors.newFixedThreadPool(10);
-    
-    private final ReentrantLock lock = new ReentrantLock();
+        @Autowired
+        private BookRepository bookRepository;
 
-	private ResponseEntity<List<Book>> list;
+        private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    @Async
-    public CompletableFuture<Book> saveBook(Book book) {
-        lock.lock();
-        try {
-            bookRepository.save(book);
-            return CompletableFuture.completedFuture(book);
-        } finally {
-        	
-            lock.unlock();
-        }
-    }
+        private final ReentrantLock lock = new ReentrantLock();
 
-    @Async
-    public CompletableFuture<Book> findBookById(Long id) {
-        lock.lock();
-        try {
-        	
-            return CompletableFuture.supplyAsync(() -> {
-                return bookRepository.findById(id).orElse(null);
-            });
-            
-            
-        } finally {
-            lock.unlock();
+        private ResponseEntity<List<Book>> list;
+
+        @Async
+        public CompletableFuture<Book> saveBook(Book book) {
+                lock.lock();
+                try {
+                        bookRepository.save(book);
+                        return CompletableFuture.completedFuture(book);
+                } finally {
+
+                        lock.unlock();
+                }
         }
-        
-    }
-    
-    @Async
-    public CompletableFuture<Book> findById(Long id) {
-        lock.lock();
-        try {
-        	
-            return CompletableFuture.supplyAsync(() -> {
-                return bookRepository.findById(id).orElse(null);
-            });
-            
-            
-        } finally {
-            lock.unlock();
+
+        @Async
+        public CompletableFuture<Book> findBookById(Long id) {
+                lock.lock();
+                try {
+
+                        return CompletableFuture.supplyAsync(() -> {
+                                return bookRepository.findById(id).orElse(null);
+                        });
+
+
+                } finally {
+                        lock.unlock();
+                }
+
         }
-        
-    }
-            
-    @Async
-    public CompletableFuture<Book> findBookByName(Long id) {
-        lock.lock();
-        CompletableFuture<Book> completableFuture = new CompletableFuture<>();
-        executorService.submit(() -> {
-        	try {
-            	
-                return CompletableFuture.supplyAsync(() -> {
-                    return bookRepository.findById(id).orElse(null);
+
+        @Async
+        public CompletableFuture<Book> findById(Long id) {
+                lock.lock();
+                try {
+
+                        return CompletableFuture.supplyAsync(() -> {
+                                return bookRepository.findById(id).orElse(null);
+                        });
+
+
+                } finally {
+                        lock.unlock();
+                }
+
+        }
+
+        @Async
+        public CompletableFuture<Book> findBookByName(Long id) {
+                lock.lock();
+                CompletableFuture<Book> completableFuture = new CompletableFuture<>();
+                executorService.submit(() -> {
+                        try {
+
+                                return CompletableFuture.supplyAsync(() -> {
+                                        return bookRepository.findById(id).orElse(null);
+                                });
+
+
+                        } finally {
+                                lock.unlock();
+                        }
                 });
-                
-                
-            } finally {
-                lock.unlock();
-            } 
-        });
-        
-        return completableFuture;
-        
-    }
-    
-    public CompletableFuture<Book> getBook(String isbn) {
-        CompletableFuture<Book> future = CompletableFuture.supplyAsync(() -> {
-            // Do some work here, e.g. call a database or make an API call.
-            Book book = new Book();
-            //book.setIsbn(isbn);
-            //book.setTitle("The Lord of the Rings");
-            //book.setAuthor("J.R.R. Tolkien");
-//            book.setName("pk");
-            return book;
-        }, executorService);
 
-        // Handle any exceptions that occur while getting the book.
-        future.exceptionally(ex -> {
-            System.out.println("Error getting book: " + ex.getMessage());
-            return null;
-        });
+                return completableFuture;
 
-        return future;
-    }
-    public List<Book> getAllBook(){
-    	
-		return new ArrayList<Book>(bookRepository.findAll());
-    }
-    
-    @Async
-    public CompletableFuture<List<Book>> getBookByCompletableFuture(){
-    	
-    	  return CompletableFuture.supplyAsync(() -> bookRepository.findAll());
-                 
-    }
-    @Async
-    public CompletableFuture<List<Book>> findAllBooks() {
-        return CompletableFuture.supplyAsync(() -> StreamSupport.stream(bookRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList()));
-    }
-    @Async
-    public CompletableFuture<Void> deleteBook(Long id) {
-        return CompletableFuture.runAsync(() -> bookRepository.deleteById(id));
-    }
-    @Async
-    public CompletableFuture<Book> updateBook(Long id, Book book) {
-        return CompletableFuture.supplyAsync(() -> {
-            Optional<Book> optionalBook = bookRepository.findById(id);
-            if (optionalBook.isPresent()) {
-                Book existingBook = optionalBook.get();
-               
-                existingBook.setName(book.getName());
-//                existingBook.setTitle(book.getTitle());
-//                existingBook.setAuthor(book.getAuthor());
-//                existingBook.setPrice(book.getPrice());
-                return bookRepository.save(existingBook);
-            }
-            return null;
-        });
-    }
-
-    public void close() {
-        executorService.shutdown();
-        try {
-            executorService.awaitTermination(10, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            System.out.println("Error closing executor service: " + e.getMessage());
         }
-    }
 
-	
-    
-	
+        public CompletableFuture<Book> getBook(String isbn) {
+                CompletableFuture<Book> future = CompletableFuture.supplyAsync(() -> {
+                        // Do some work here, e.g. call a database or make an API call.
+                        Book book = new Book();
+                        //book.setIsbn(isbn);
+                        //book.setTitle("The Lord of the Rings");
+                        //book.setAuthor("J.R.R. Tolkien");
+                        // book.setName("pk");
+                        return book;
+                }, executorService);
+
+                // Handle any exceptions that occur while getting the book.
+                future.exceptionally(ex -> {
+                        System.out.println("Error getting book: " + ex.getMessage());
+                        return null;
+                });
+
+                return future;
+        }
+        @Async
+        public List<Book> getAllBook() {
+
+                return new ArrayList<Book>(bookRepository.findAll());
+        }
+
+        @Async
+        public CompletableFuture<List<Book>> getBookByCompletableFuture() {
+
+                return CompletableFuture.supplyAsync(() -> bookRepository.findAll());
+
+        }
+
+        @Async
+        public CompletableFuture<List<Book>> findAllBooks() {
+                return CompletableFuture.supplyAsync(() -> StreamSupport.stream(bookRepository.findAll().spliterator(), false)
+                        .collect(Collectors.toList()));
+        }
+
+        @Async
+        public CompletableFuture<Void> deleteBook(Long id) {
+                return CompletableFuture.runAsync(() -> bookRepository.deleteById(id));
+        }
+
+        @Async
+        public CompletableFuture<Book> updateBook(Long id, Book book) {
+                return CompletableFuture.supplyAsync(() -> {
+                        Optional<Book> optionalBook = bookRepository.findById(id);
+                        if (optionalBook.isPresent()) {
+                                Book existingBook = optionalBook.get();
+
+                                existingBook.setName(book.getName());
+                                return bookRepository.save(existingBook);
+                        }
+                        return null;
+                });
+        }
+
+        public void close() {
+                executorService.shutdown();
+                try {
+                        executorService.awaitTermination(10, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                        System.out.println("Error closing executor service: " + e.getMessage());
+                }
+        }
+
 
 }
